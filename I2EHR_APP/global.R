c7db557a843fd36f686730120379c4c042947861
Shane Crinion <shanecrinion@gmail.com>
2019-05-15 16:05:48 +0100

    Arranging app files and all work into different tabs

---
+++ I2EHR_APP/global.R
@@ -0,0 +1,12 @@
+setwd("patient_files/")
+
+#import csv files
+temp = list.files(pattern="*.csv")
+for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i]))
+
+
+#merging the data
+library(plyr)
+clinical_data <- ldply(.data = list.files(pattern="*.csv"),
+                       .fun = read.csv,
+                       header=TRUE)
