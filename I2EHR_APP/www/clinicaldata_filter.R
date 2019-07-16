a280c639fb6af85b3e269ddcedb6652ba3cc7f46
Shane Crinion <shanecrinion@gmail.com>
2019-07-16 22:55:58 +0100

    Final work

--- I2EHR_APP/www/clinicaldata_filter.R
+++ I2EHR_APP/www/clinicaldata_filter.R
@@ -2,22 +2,23 @@
 observations.csv <-with(observations.csv , observations.csv[order(PATIENT),])
 
 # list 64 patients with most obs (match no of GEO samples)
-patients_most_obs <- unique(observations.csv$PATIENT)[0:160]
+patients_most_obs <- unique(observations.csv$PATIENT)
+
+observations.csv <- subset(observations.csv, observations.csv$PATIENT %in% patients.csv$PATIENT)
 
 # format patients.csv from Id to PATIENT to match other files
 names(patients.csv)[names(patients.csv) == "Id"] <- "PATIENT"
 
 # limit the info in each other csv file to these patients
-allergies.csv <- allergies.csv[allergies.csv$PATIENT %in% patients_most_obs,]
-careplans.csv <- careplans.csv[careplans.csv$PATIENT %in% patients_most_obs,]
-conditions.csv <- conditions.csv[conditions.csv$PATIENT %in% patients_most_obs,]
-encounters.csv <- encounters.csv[encounters.csv$PATIENT %in% patients_most_obs,]
-immunizations.csv <- immunizations.csv[immunizations.csv$PATIENT %in% patients_most_obs,]
-observations.csv <- observations.csv[observations.csv$PATIENT %in% patients_most_obs,]
-organizations.csv <- organizations.csv[organizations.csv$PATIENT %in% patients_most_obs,]
-patients.csv <- patients.csv[patients.csv$PATIENT %in% patients_most_obs,]
-procedures.csv <- procedures.csv[procedures.csv$PATIENT %in% patients_most_obs,]
-providers.csv <- providers.csv[providers.csv$PATIENT %in% patients_most_obs,]
+allergies.csv <- subset(allergies.csv, allergies.csv$PATIENT %in% patients.csv$PATIENT)
+careplans.csv <- subset(careplans.csv, careplans.csv$PATIENT %in% patients.csv$PATIENT)
+conditions.csv <- subset(conditions.csv, conditions.csv$PATIENT %in% patients.csv$PATIENT)
+encounters.csv <- subset(encounters.csv, encounters.csv$PATIENT %in% patients.csv$PATIENT)
+immunizations.csv <- subset(immunizations.csv, immunizations.csv$PATIENT %in% patients.csv$PATIENT)
+observations.csv <- subset(observations.csv, observations.csv$PATIENT %in% patients.csv$PATIENT)
+organizations.csv <- subset(organizations.csv, organizations.csv$PATIENT %in% patients.csv$PATIENT)
+procedures.csv <- subset(procedures.csv, procedures.csv$PATIENT %in% patients.csv$PATIENT)
+providers.csv <- subset(providers.csv, providers.csv$PATIENT %in% patients.csv$PATIENT)
 
 
 write.csv(allergies.csv, file = "allergies.csv")
